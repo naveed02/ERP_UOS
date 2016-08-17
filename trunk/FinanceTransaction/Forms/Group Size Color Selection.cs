11be599460adc5f2af5d8a30b49b6aa9c5079861
ahsansaeeed <syedahsansaeed@gmail.com>
2016-08-17 15:09:35 +0500

    changes

--- trunk/FinanceTransaction/Forms/Group Size Color Selection.cs
+++ trunk/FinanceTransaction/Forms/Group Size Color Selection.cs
@@ -11,6 +11,7 @@ namespace ERP_UOS
 {
     public partial class frmGroup : Form
     {
+        int fcboDefaultValue = 0;
         public frmGroup()
         {
             InitializeComponent();
@@ -19,6 +20,20 @@ namespace ERP_UOS
         private void frmGroup_Load(object sender, EventArgs e)
         {
             this.MaximizeBox = false;
+             AtFormLoad();
+        }
+
+        private void AtFormLoad()
+        {
+            string lSQL = string.Empty;
+
+            lSQL = "SELECT cgdCode, cgdDesc "
+                 + " FROM CatDtl "
+                 + " WHERE cgCode = 6 "
+                 + " ORDER BY cgdDesc";
+
+            clsFillCombo.FillCombo(cboGroup, clsGVar.ConString1, "CatDtl" + "," + "cgdCode" + "," + "False", lSQL, true);
+            fcboDefaultValue = Convert.ToInt16(cboGroup.SelectedValue);
         }
     }
      
