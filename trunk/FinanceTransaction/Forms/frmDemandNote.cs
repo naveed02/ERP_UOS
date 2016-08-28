0be49b9ecbddbde57505b34b039bf77ef9bef84c
hasan04 <hasanking990099@gmail.com>
2016-08-28 07:00:59 +0500

    cboFilling

--- trunk/FinanceTransaction/Forms/frmDemandNote.cs
+++ trunk/FinanceTransaction/Forms/frmDemandNote.cs
@@ -11,6 +11,7 @@ namespace ERP_UOS
 {
     public partial class frmDemandNote : Form
     {
+        int fcboDefaultValue = 0;
         
         public frmDemandNote()
         {
@@ -23,8 +24,35 @@ namespace ERP_UOS
         {
 
             this.MaximizeBox = false;
-            
+            AtfrmLoad();
         }
+
+
+        private void AtfrmLoad()
+        {
+            string ISQL = string.Empty;
+            ISQL = "SELECT cgdCode, cgdDesc"
+             + " FROM CatDtl"
+             + " WHERE cgCode = 2"
+             + " ORDER BY cgdDesc";
+
+
+            clsFillCombo.FillCombo(cboGodownForBal, clsGVar.ConString1, "CatDtl" + "," + "cgdCode" + "," + "False", ISQL, true);
+            fcboDefaultValue = Convert.ToInt16(cboGodownForBal.SelectedValue);
+
+
+            ISQL = "SELECT cgdCode, cgdDesc"
+             + " FROM CatDtl"
+             + " WHERE cgCode = 6"
+             + " ORDER BY cgdDesc";
+
+
+            clsFillCombo.FillCombo(cboItemGroup, clsGVar.ConString1, "CatDtl" + "," + "cgdCode" + "," + "False", ISQL, true);
+            fcboDefaultValue = Convert.ToInt16(cboItemGroup.SelectedValue);
+
+        }
+
+
 
        
 
