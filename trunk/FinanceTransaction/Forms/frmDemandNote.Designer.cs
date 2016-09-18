dbd95dcee82fe349646bda2400f1bfdf8ba46595
hasan04 <hasanking990099@gmail.com>
2016-09-18 11:54:07 +0500

    DemandNoteHelpForm

--- trunk/FinanceTransaction/Forms/frmDemandNote.Designer.cs
+++ trunk/FinanceTransaction/Forms/frmDemandNote.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.lblDemandNoteNumber = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnF1Help = new System.Windows.Forms.Button();
             this.optSingleItemGroup = new System.Windows.Forms.RadioButton();
             this.optMultiItemGroup = new System.Windows.Forms.RadioButton();
             this.cboDepartment = new System.Windows.Forms.ComboBox();
@@ -71,6 +70,7 @@
             this.optSimple = new System.Windows.Forms.RadioButton();
             this.optWithGRNHistory = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDemandNoteNumber = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grdDemandNote)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -142,18 +142,6 @@
             this.label6.Text = "Note";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblDemandNoteNumber
-            // 
-            this.lblDemandNoteNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.lblDemandNoteNumber.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblDemandNoteNumber.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDemandNoteNumber.Location = new System.Drawing.Point(185, 5);
-            this.lblDemandNoteNumber.Name = "lblDemandNoteNumber";
-            this.lblDemandNoteNumber.Size = new System.Drawing.Size(117, 23);
-            this.lblDemandNoteNumber.TabIndex = 0;
-            this.lblDemandNoteNumber.Text = "01-16-1";
-            this.lblDemandNoteNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // label7
             // 
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -175,15 +163,16 @@
             this.button1.Text = "&Add New";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnF1Help
             // 
-            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(566, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "F1=Help";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnF1Help.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnF1Help.Location = new System.Drawing.Point(566, 2);
+            this.btnF1Help.Name = "btnF1Help";
+            this.btnF1Help.Size = new System.Drawing.Size(87, 23);
+            this.btnF1Help.TabIndex = 9;
+            this.btnF1Help.Text = "F1=Help";
+            this.btnF1Help.UseVisualStyleBackColor = true;
+            this.btnF1Help.Click += new System.EventHandler(this.btnF1Help_Click);
             // 
             // optSingleItemGroup
             // 
@@ -287,16 +276,16 @@
             // 
             // Column1
             // 
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle3;
             this.Column1.HeaderText = "Code";
             this.Column1.Name = "Column1";
             this.Column1.Width = 60;
             // 
             // Column2
             // 
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle4;
             this.Column2.HeaderText = "Item Code";
             this.Column2.Name = "Column2";
             this.Column2.Width = 90;
@@ -471,11 +460,21 @@
             this.groupBox2.TabIndex = 36;
             this.groupBox2.TabStop = false;
             // 
+            // txtDemandNoteNumber
+            // 
+            this.txtDemandNoteNumber.Location = new System.Drawing.Point(186, 5);
+            this.txtDemandNoteNumber.Name = "txtDemandNoteNumber";
+            this.txtDemandNoteNumber.Size = new System.Drawing.Size(138, 21);
+            this.txtDemandNoteNumber.TabIndex = 37;
+            this.txtDemandNoteNumber.DoubleClick += new System.EventHandler(this.txtDemandNoteNumber_DoubleClick);
+            this.txtDemandNoteNumber.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDemandNoteNumber_KeyDown);
+            // 
             // frmDemandNote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(983, 484);
+            this.Controls.Add(this.txtDemandNoteNumber);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.optApprovedVerified);
             this.Controls.Add(this.optInProcess);
@@ -494,10 +493,9 @@
             this.Controls.Add(this.cboItemGroup);
             this.Controls.Add(this.cboEmpCode);
             this.Controls.Add(this.cboDepartment);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnF1Help);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.lblDemandNoteNumber);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -527,10 +525,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label lblDemandNoteNumber;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnF1Help;
         private System.Windows.Forms.RadioButton optSingleItemGroup;
         private System.Windows.Forms.RadioButton optMultiItemGroup;
         private System.Windows.Forms.ComboBox cboDepartment;
@@ -562,5 +559,6 @@
         private System.Windows.Forms.RadioButton optSimple;
         private System.Windows.Forms.RadioButton optWithGRNHistory;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDemandNoteNumber;
     }
 }
--- trunk/FinanceTransaction/Forms/frmDemandNote.cs
+++ trunk/FinanceTransaction/Forms/frmDemandNote.cs
@@ -79,8 +79,86 @@ namespace ERP_UOS
             this.Close();
         }
 
+ private void LookUp_Voc()
+        {
+            // 1- KeyField
+            // 2- Field List
+            // 3- Table Name
+            // 4- Form Title
+            // 5- Default Find Field (Int) 0,1,2,3 etc Default = 1 = title field
+            // 6- Grid Title List
+            // 7- Grid Title Width
+            // 8- Grid Title format T = Text, N = Numeric etc
+            // 9- Bool One Table = True, More Then One = False
+            // 10 Join String Otherwise Empty String.
+            // 11 Optional Where
+            // 11 Return Control Type TextBox or MaskedTextBox Default mtextBox
+            //
 
+//select DNId,  Date, ItemGroupId, DepartmentId,EmployeeId, Note from DN
+       
 
+            frmLookUp sForm = new frmLookUp(
+                    " DNId",
+                    " Date, ItemGroupId, DepartmentId, EmployeeId, Note",
+                    " DN",
+                    this.Text.ToString(),
+                    1,
+                    "DN #, DN Date, Item Group, Department, Employee Name, Note",
+                    "15,15,15,15,15,15",
+                    " T, T, T, T,T,T",
+                    true,
+                    "",
+                    "",
+                    "TextBox"
+                    );
+            txtDemandNoteNumber.Text = string.Empty;
+            sForm.lupassControl = new frmLookUp.LUPassControl(PassDataVocID);
+            sForm.ShowDialog();
+            if (txtDemandNoteNumber.Text != null)
+            {
+                if (txtDemandNoteNumber.Text != null)
+                {
+                    if (txtDemandNoteNumber.Text.ToString() == "" || txtDemandNoteNumber.Text.ToString() == string.Empty)
+                    {
+                        return;
+                    }
+                    if (txtDemandNoteNumber.Text.ToString().Trim().Length > 0)
+                    {
+                        //PopulateRecords();
+                        //SumVoc();
+                    }
+                }
+            }
+        }
+
+            private void PassDataVocID(object sender)
+       {
+           txtDemandNoteNumber.Text = ((TextBox)sender).Text;
+
+       }
+
+            
+
+            private void btnF1Help_Click(object sender, EventArgs e)
+            {
+                LookUp_Voc();
+            }
+
+            private void txtDemandNoteNumber_DoubleClick(object sender, EventArgs e)
+            {
+                LookUp_Voc();
+            }
+
+            private void txtDemandNoteNumber_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (e.KeyCode == Keys.F1)
+                {
+                    LookUp_Voc();
+                }
+            }
+
+          
 
     }
 }
